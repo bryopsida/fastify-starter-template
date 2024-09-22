d2e35cd80a0484e91801ab7e0c66ae496d1fa891
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-09-22 09:00:23 -0500

    add swagger

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 import { randomBytes } from 'node:crypto'
+import { totalmem } from 'node:os'
 
 import 'dotenv/config'
 import Fastify from 'fastify'
@@ -8,8 +9,13 @@ import etag from '@fastify/etag'
 import secureSession from '@fastify/secure-session'
 import csrfProtection from '@fastify/csrf-protection'
 import underPressure from '@fastify/under-pressure'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 
 export default function builder (opts) {
+  const totalMemory = totalmem()
+  const ninetyFivePercentMemory = totalMemory * 0.95
+
   const app = Fastify(opts)
   app.register(helmet)
   app.register(etag)
@@ -19,10 +25,42 @@ export default function builder (opts) {
   app.register(csrfProtection, { sessionPlugin: '@fastify/secure-session' })
   app.register(underPressure, {
     maxEventLoopDelay: process.env.FASTIFY_UNDER_PRESSURE_MAX_LOOP_DELAY ?? 1000,
-    maxHeapUsedBytes: process.env.FASTIFY_UNDER_PRESSURE_MAX_HEAP_USED_BYTES ?? 100000000,
-    maxRssBytes: process.env.FASTIFY_UNDER_PRESSURE_MAX_RSS_BYTES ?? 100000000,
+    maxHeapUsedBytes: process.env.FASTIFY_UNDER_PRESSURE_MAX_HEAP_USED_BYTES ?? ninetyFivePercentMemory,
+    maxRssBytes: process.env.FASTIFY_UNDER_PRESSURE_MAX_RSS_BYTES ?? ninetyFivePercentMemory,
     maxEventLoopUtilization: process.env.FASTIFY_UNDER_PRESSURE_MAX_LOOP_UTIL ?? 0.98,
     exposeStatusRoute: true
+  })
+  app.register(swagger, {
+    openapi: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Fastify Starter template',
+        description: 'A starter template for a fastify service',
+        version: '0.1.0'
+      },
+      servers: [],
+      tags: [],
+      components: {},
+      externalDocs: {
+        url: 'https://github.com/bryopsida/fastify-starter-template#README',
+        description: 'Find more info here'
+      }
+    }
+  })
+  app.register(swaggerUi, {
+    routePrefix: '/documentation',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
   })
 
   if (!process.env.FASTIFY_SESSION_SECRET) {
